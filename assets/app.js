95826e1e996af6b5a7b781eebb7582eb634f65a7
Hossain79 <59659882+Hossain79@users.noreply.github.com>
2020-10-17 03:53:16 +0200

    files added

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+const mobNav = document.querySelector(".mob-nav");
+const hamburger = document.querySelector(".nav-icon");
+
+
+hamburger.addEventListener('click', ()=>
+mobNav.classList.toggle("isShow")
+);
